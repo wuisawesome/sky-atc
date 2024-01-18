ce41613d7d8e8c5ad49fc009b3356b34cd2c9158
Alex <alexanderwu@berkeley.edu>
2024-01-17 19:58:51 -0500

    get stockStatus for runpod gpu querier

--- src/python/sky_atc/runpod/gpu_querier.py
+++ src/python/sky_atc/runpod/gpu_querier.py
@@ -12,6 +12,70 @@ import runpod.api.ctl_commands
 import sys
 import time
 import pandas as pd
+import requests
+
+def get_gpu_stock_status(id : str, secure : bool):
+    secure_gpus_graphql_request = {
+        "operationName":"SecureGpuTypes",
+        "variables":{
+            "gpuTypesInput":{
+                "id": None,
+            },
+            "lowestPriceInput":
+            {
+                "dataCenterId":None,
+                "gpuCount":1,
+                "minDisk":0,
+                "minMemoryInGb":8,
+                "minVcpuCount":2,
+                "secureCloud": secure
+            }
+        },
+        "query":
+        """query SecureGpuTypes($lowestPriceInput: GpuLowestPriceInput, $gpuTypesInput: GpuTypeFilter) {
+  gpuTypes(input: $gpuTypesInput) {
+    lowestPrice(input: $lowestPriceInput) {
+      minimumBidPrice
+      uninterruptablePrice
+      minVcpu
+      minMemory
+      stockStatus
+      __typename
+    }
+    id
+    displayName
+    memoryInGb
+    securePrice
+    communityPrice
+    oneMonthPrice
+    threeMonthPrice
+    sixMonthPrice
+    secureSpotPrice
+    __typename
+  }
+}"""
+    }
+
+    # If we just didn't specify this we would get all the gpus 🤔. Maybe we
+    # should do that? For now, we'll do it this way since the website does
+    # it this way. Maybe smaller requests are better for their load?
+    secure_gpus_graphql_request["variables"]["gpuTypesInput"]["id"] = id
+    result = requests.post(
+        f"https://api.runpod.io/graphql?api_key={runpod.api_key}",
+        json = secure_gpus_graphql_request
+    )
+
+    assert result.ok, f"Result not ok {result.status_code=}, {result.text=}"
+
+    gpu_types = result.json()["data"]["gpuTypes"]
+
+    assert len(gpu_types) == 1, f"Multiple gpu types {gpu_types}"
+
+    gpu_details = gpu_types[0]
+
+    assert gpu_details["id"] == id
+
+    return gpu_details["lowestPrice"]["stockStatus"]
 
 def get_all_gpu_details() -> pd.DataFrame:
     all_gpu_details = {
@@ -28,12 +92,16 @@ def get_all_gpu_details() -> pd.DataFrame:
         'oneWeekPrice': [], # float
         'oneMonthPrice': [], # float
         'threeMonthPrice': [], #float
+        'secureStockStatus': [], #str
+        'communityStockStatus': [] # str
     }
 
     gpus = runpod.api.ctl_commands.get_gpus()
     for gpu in gpus:
         id = gpu["id"]
         gpu_details = runpod.api.ctl_commands.get_gpu(id)
+        gpu_details["secureStockStatus"] = get_gpu_stock_status(id, secure=True)
+        gpu_details["communityStockStatus"] = get_gpu_stock_status(id, secure=False)
         gpu_details["timestamp"] = time.time()
 
         for key in all_gpu_details:
